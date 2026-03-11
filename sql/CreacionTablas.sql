567be7d8744a9715e662c80b77bb64672e5a3714
Miriam Fernández <myriam1869@gmail.com>
2026-03-11 17:16:57 +0100

    Actualizado script SQL creacion de tablas
    Se ha modificado por un archivo generado manualmente.

--- sql/CreacionTablas.sql
+++ sql/CreacionTablas.sql
@@ -1,176 +1,48 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: localhost
--- Tiempo de generación: 11-03-2026 a las 15:33:12
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+create table clientes (
+    id_cliente int AUTO_INCREMENT PRIMARY KEY,
+    nombre varchar(50) not null,
+    telefono varchar(50) not null,
+    email varchar(100)
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+create table servicios (
+    id_servicio int AUTO_INCREMENT PRIMARY KEY,
+    nombre varchar(50) not null,
+    precio decimal(3,2),
+    duracion INT
+);
 
+create table empleados (
+    id_empleado int AUTO_INCREMENT PRIMARY KEY,
+    nombre varchar(50) not null,
+    especialidad varchar(50)
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+create table cita (
+    id_cita int AUTO_INCREMENT PRIMARY KEY,
+    fecha date not null,
+    hora time not null,
+    
+    id_cliente int,
+    id_servicio int,
+    id_empleado int,
+    
+    foreign key (id_cliente) references clientes(id_cliente),
+    foreign key (id_servicio) references servicios(id_servicio),
+    foreign key (id_empleado) references empleados(id_empleado)
+    
+    on delete restrict
+    on update cascade
+);
 
---
--- Base de datos: `Belladonna`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `cita`
---
-
-CREATE TABLE `cita` (
-  `id_cita` int(11) NOT NULL,
-  `fecha` date NOT NULL,
-  `hora` time NOT NULL,
-  `id_cliente` int(11) DEFAULT NULL,
-  `id_servicio` int(11) DEFAULT NULL,
-  `id_empleado` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `clientes`
---
-
-CREATE TABLE `clientes` (
-  `id_cliente` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `telefono` varchar(50) NOT NULL,
-  `email` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `empleados`
---
-
-CREATE TABLE `empleados` (
-  `id_empleado` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `especialidad` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `servicios`
---
-
-CREATE TABLE `servicios` (
-  `id_servicio` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `precio` decimal(3,2) DEFAULT NULL,
-  `duracion` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `servicio_empleado`
---
-
-CREATE TABLE `servicio_empleado` (
-  `id_empleado` int(11) NOT NULL,
-  `id_servicio` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `cita`
---
-ALTER TABLE `cita`
-  ADD PRIMARY KEY (`id_cita`),
-  ADD KEY `id_cliente` (`id_cliente`),
-  ADD KEY `id_servicio` (`id_servicio`),
-  ADD KEY `id_empleado` (`id_empleado`);
-
---
--- Indices de la tabla `clientes`
---
-ALTER TABLE `clientes`
-  ADD PRIMARY KEY (`id_cliente`);
-
---
--- Indices de la tabla `empleados`
---
-ALTER TABLE `empleados`
-  ADD PRIMARY KEY (`id_empleado`);
-
---
--- Indices de la tabla `servicios`
---
-ALTER TABLE `servicios`
-  ADD PRIMARY KEY (`id_servicio`);
-
---
--- Indices de la tabla `servicio_empleado`
---
-ALTER TABLE `servicio_empleado`
-  ADD PRIMARY KEY (`id_empleado`,`id_servicio`),
-  ADD KEY `id_servicio` (`id_servicio`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `cita`
---
-ALTER TABLE `cita`
-  MODIFY `id_cita` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `clientes`
---
-ALTER TABLE `clientes`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `empleados`
---
-ALTER TABLE `empleados`
-  MODIFY `id_empleado` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `servicios`
---
-ALTER TABLE `servicios`
-  MODIFY `id_servicio` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `cita`
---
-ALTER TABLE `cita`
-  ADD CONSTRAINT `cita_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`),
-  ADD CONSTRAINT `cita_ibfk_2` FOREIGN KEY (`id_servicio`) REFERENCES `servicios` (`id_servicio`),
-  ADD CONSTRAINT `cita_ibfk_3` FOREIGN KEY (`id_empleado`) REFERENCES `empleados` (`id_empleado`) ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `servicio_empleado`
---
-ALTER TABLE `servicio_empleado`
-  ADD CONSTRAINT `servicio_empleado_ibfk_1` FOREIGN KEY (`id_empleado`) REFERENCES `empleados` (`id_empleado`),
-  ADD CONSTRAINT `servicio_empleado_ibfk_2` FOREIGN KEY (`id_servicio`) REFERENCES `servicios` (`id_servicio`) ON UPDATE CASCADE;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+create table servicio_empleado (
+    id_empleado int,
+    id_servicio int,
+    
+    primary key (id_empleado, id_servicio),
+    foreign key (id_empleado) references empleados(id_empleado),
+    foreign key (id_servicio) references servicios(id_servicio)
+    
+    on delete RESTRICT
+    on update CASCADE
+);
